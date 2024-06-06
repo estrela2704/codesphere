378f5fbc7581161840ce2c215c4118ac1c8f8b25
Felipe Estrela <felipeestrelatv@gmail.com>
2024-06-06 20:38:53 -0300

    UI/UX modifications

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,84 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bem-vindo</title>
-    <!-- Estilos do Breeze -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'CodeSphere') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f7fafc;
-        }
 
-        .logo-container {
-            text-align: center;
-            margin-top: 50px;
-        }
-
-        .logo {
-            max-width: 200px;
-        }
-
-        .welcome-text {
-            text-align: center;
-            margin-top: 30px;
-            font-size: 20px;
-            color: #4a5568;
-        }
-
-        .button-container {
-            text-align: center;
-            margin-top: 30px;
-        }
-
-        .login-button {
-            display: inline-block;
-            padding: 10px 20px;
-            background-color: #60a5fa;
-            /* Cor de fundo do botão de login */
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            font-size: 16px;
-            cursor: pointer;
-            transition: background-color 0.3s ease;
-            text-decoration: none;
-        }
-
-        .login-button:hover {
-            background-color: #3b82f6;
-            /* Cor de fundo do botão de login quando hover */
-        }
-
-        .register-link {
-            margin-top: 10px;
-            font-size: 14px;
-            color: #4a5568;
-            text-decoration: none;
-        }
-
-        .register-link:hover {
-            color: #718096;
-        }
-    </style>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-    <div class="logo-container">
-        <img src="{{ asset('img/logo.png') }}" alt="Logo" class="logo">
-    </div>
+<body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div>
+            <a href="/">
+                <img src="{{ asset('img/logo.png') }}" width="200px" alt="Logo" class="logo mt-5">
+            </a>
+        </div>
 
-    <div class="welcome-text">
-        <h2>Bem-vindo ao nosso aplicativo!</h2>
-        <p>Faça login ou registre-se para continuar.</p>
-    </div>
+        <div class="welcome-text m-9">
+            <h2 class="text-2xl font-medium text-gray-900">Bem-vindo ao nosso Website!</h2>
+            <p class="mt-2 text-xl text-gray-500 mt-5">Entre ou registre-se para continuar.</p>
+        </div>
 
-    <div class="button-container">
-        <a href="{{ route('login') }}" class="login-button">Login</a>
-        <p class="register-link">Não tem uma conta? <a href="{{ route('register') }}">Registre-se</a></p>
+        <div class="button-container mt-1 flex justify-between">
+            <x-primary-button class="ms-4">
+                <a href="{{ route('login') }}">
+                    Entrar
+                </a>
+            </x-primary-button>
+            <x-primary-button class="ms-4">
+                <a href="{{ route('register') }}">
+                    Registrar
+                </a>
+            </x-primary-button>
+        </div>
     </div>
 </body>
 
