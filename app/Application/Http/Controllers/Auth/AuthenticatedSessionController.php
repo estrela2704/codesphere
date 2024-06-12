50b3ffb43b83c3f17e33d35d18636fc2a47cfda8
Felipe Estrela <felipeestrelatv@gmail.com>
2024-06-11 22:26:00 -0300

    traduzindo retorno de erros

--- app/Application/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Application/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Illuminate\Validation\ValidationException;
+
 
 class AuthenticatedSessionController extends Controller
 {
@@ -25,7 +27,6 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
-
 
         $request->session()->regenerate();
 
